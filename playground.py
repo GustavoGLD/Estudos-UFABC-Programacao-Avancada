713a86546884e605c8b213d06386b37623624485
gustavogld <gustavo.relativistico@gmail.com>
2025-02-19 11:45:17 -0300

    update playground.py

--- playground.py
+++ playground.py
@@ -1,43 +1,32 @@
-class Node:
-	def __init__(self, d):
-		self.data = d
-		self.next_node = None
-
-class Stack:
-	def __init__(self):
-		self.top = None
-		self.size = 0
-	def push(self, d):
-		new_node = Node(d)
-		if self.top:
-			new_node.next_node = self.top
-		self.top = new_node
-		self.size += 1
-	def pop(self):
-		if self.top is None:
-			return None
-		result = self.top.data
-		self.top = self.top.next_node
-		self.size -= 1
-		return result
-	def peek(self):
-		if self.top is None:
-			return None
-		return self.top.data
-	def is_empty(self):
-		if self.top is None:
-			return True
-		else:
-			return False
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.pop()
-stack.push(4)
+class Queue(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-while not stack.is_empty():
-	print(stack.peek())
-	stack.pop()
+    def isEmpty(self):
+        return self.head == None
+
+    def peek(self):
+        return self.head.data
+
+    def enqueue(self, data):
+        new_data = Node(data)
+        if self.head is None:
+            self.head = new_data
+            self.tail = self.head
+        else:
+            self.tail.next = new_data
+            self.tail = new_data
+
+    def dequeue(self):
+        data = self.head.data
+        self.head = self.head.next
+        if self.head is None:
+            self.tail = None
+        return data
